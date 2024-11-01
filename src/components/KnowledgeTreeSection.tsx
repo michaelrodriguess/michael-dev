4e4fe0db1baaa50bf6b75f30e6ce03be1e0367dd
Michael Rodrigues <run.michael97@gmail.com>
2024-10-31 21:02:58 -0300

    chore: handle with display label

--- src/components/KnowledgeTreeSection.tsx
+++ src/components/KnowledgeTreeSection.tsx
@@ -4,10 +4,7 @@ import MultiverseScene from "./MultiverseScene";
 
 export default function KnowledgeTreeSection() {
   return (
-    <section className="py-5 sm:py-10 lg:py-10 flex flex-col items-center justify-center">
-      <h2 className="text-2xl sm:text-3xl lg:text-4xl font-bold mb-6 text-center text-[#F96458]">
-        Knowledge Tree
-      </h2>
+    <section className="flex flex-col items-center justify-center">
       <div className="border-2 border-[#FFC680] rounded-xl w-full max-w-3xl h-[300px] sm:h-[400px] lg:h-[500px] flex items-center justify-center bg-black">
         <MultiverseScene data={projectData} />
       </div>
